0f605b0f52205d7c52a8c2c961f1dba14deab21b
Your Name <your.email@example.com>
2019-11-07 09:39:37 +0000

    heroku test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "hello, world!"
+    render html: "hell0000000000000000o, world!!!!!!!!!!!!!!!"
   end
 end
