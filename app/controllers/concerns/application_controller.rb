9075af0c45b75de1060079716d3d6fba847c6e16
Your Name <your.email@example.com>
2019-11-15 08:51:29 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/concerns/application_controller.rb
+++ app/controllers/concerns/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
-  def hello
-    render html: "hell0000000000000000o, world!!!!!!!!!!!!!!!"
-  end
+  include SessionsHelper
 end
